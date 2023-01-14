cc65b66849f2ae5ad088ff9f0a593080b4869fc5
Hanzian <114009272+Hanzian@users.noreply.github.com>
2023-01-14 10:32:43 -0500

    Add files via upload

---
+++ Earthquakes_past7days/static/js/logic3.js
@@ -0,0 +1,110 @@
+
+console.log("working")
+
+// create a layer 
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  "Streets": streets,
+  "Satellite": satelliteStreets
+};
+
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Add GeoJSON data.
+// Accessing the airport GeoJSON URL
+let earthquakePastSeven = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Create a style for the lines.
+// This function returns the style data for each of the earthquakes we plot on
+// the map. We pass the magnitude of the earthquake into a function
+// to calculate the radius.
+function styleInfo(feature) {
+  return {
+    opacity: 1,
+    fillOpacity: 1,
+    fillColor: getColor(feature.properties.mag),
+    color: "#000000",
+    radius: getRadius(feature.properties.mag),
+    stroke: true,
+    weight: 0.5
+  };
+}
+
+// This function determines the color of the circle based on the magnitude of the earthquake.
+function getColor(magnitude) {
+  if (magnitude > 5) {
+    return "#ff0000";
+  } else if (magnitude > 4) {
+    return "#ff6600";
+  } else if (magnitude > 3) {
+    return "#ff9900";
+  } else if (magnitude > 2) {
+    return "#ffcc00";
+  } else if (magnitude > 1) {
+    return "#ffff00";
+  } else {
+    return "#00ff00";
+  }
+}
+
+
+// This function determines the radius of the earthquake marker based on its magnitude.
+// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+function getRadius(magnitude) {
+  if (magnitude === 0) {
+    return 1;
+  }
+  return magnitude * 4;
+}
+
+// Grabbing our GeoJSON data.
+d3.json(earthquakePastSeven).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data, {
+// We turn each feature into a circleMarker on the map.
+pointToLayer: function(feature, latlng) {
+  console.log(data);
+  return L.circleMarker(latlng);
+},
+style : styleInfo,
+// We create a popup for each circleMarker to display the magnitude and
+    //  location of the earthquake after the marker has been created and styled.
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+  }).addTo(map);
+});
+// // Grabbing our GeoJSON data.
+// d3.json(earthquakePastSeven).then(function(data) {
+//     console.log(data);
+//   // Creating a GeoJSON layer with the retrieved data.
+//   L.geoJSON(data, {
+//     style: myStyle,
+//     onEachFeature: function(feature, layer){
+//         layer.bindPopup("<h3 Airline>" + feature.properties.airline + "</h3>")
+//     }
+//   }).addTo(map);
+// });
